eb2149d366bc4073a02908c36bebf72c6eb4e673
Le Ba Toan <toanlb.bhsoft@gmail.com>
2022-11-03 16:56:27 +0700

    First commit

---
+++ cypress/e2e/products.cy.ts
@@ -0,0 +1,85 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import cypress from 'cypress';
+
+describe('Product page', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/products');
+    cy.intercept('POST', '**/products').as('addProductAPI');
+    cy.intercept('GET', '**/products').as('getListProductAPI');
+    cy.intercept('DELETE', '**/products/**').as('deleteProductAPI');
+    cy.wait(1000);
+  });
+
+  it('add product', () => {
+    cy.get('h6').contains('Add product').should('be.visible');
+    cy.get("input[name='name']").should('be.visible');
+    cy.get("input[name='price']").should('be.visible');
+    cy.get('button').contains('Add').should('be.visible');
+    const testName = 'test product ' + Date.now() / 1000;
+    cy.get("input[name='name']").type(testName);
+    cy.get("input[name='price']").type('10000');
+
+    cy.get('button').contains('Add').click();
+
+    cy.wait('@addProductAPI').then(({response}) => {
+      expect(response?.statusCode).to.eq(201);
+      cy.wait('@getListProductAPI').then(() => {
+        cy.wait(1000);
+        cy.get('tr')
+          .last()
+          .within(() => {
+            cy.get('td').first().should('contain.text', testName);
+          });
+      });
+    });
+  });
+
+  it('delete product', () => {
+    let productNameToDelete;
+    cy.get('tr')
+      .last()
+      .within(() => {
+        cy.get('td')
+          .first()
+          .invoke('text')
+          .then((text) => (productNameToDelete = text));
+        cy.get('button').contains('Delete').click();
+      });
+    cy.wait('@deleteProductAPI').then(({response}) => {
+      expect(response?.statusCode).to.eq(200);
+      cy.wait('@getListProductAPI').then(() => {
+        cy.get('tr')
+          .last()
+          .within(() => {
+            cy.get('td')
+              .first()
+              .should('not.contain.text', productNameToDelete);
+          });
+      });
+    });
+  });
+});
+
+describe('Product page empty data ', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/products');
+
+    cy.intercept('GET', '**/products', {
+      body: [],
+    }).as('emptyData');
+
+    cy.intercept('GET', '**/products', {
+      statusCode: 404,
+    }).as('apiError');
+
+    cy.wait(1500);
+  });
+
+  it('Product list empty: []', () => {
+    cy.wait('@emptyData').then(({response}) => {
+      cy.log('response', response);
+    });
+
+    cy.get('table tbody').should('have.text', '');
+  });
+});
